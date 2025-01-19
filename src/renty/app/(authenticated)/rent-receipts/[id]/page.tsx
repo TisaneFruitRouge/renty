20d8debe1a99234b8a2bbb75fce3d5f9330d9a61
Vincent WENDLING <vincent.wendling@lilo.org>
2025-01-19 22:48:01 +0100

    feat: add rent receipt details page

--- src/renty/app/(authenticated)/rent-receipts/[id]/page.tsx
+++ src/renty/app/(authenticated)/rent-receipts/[id]/page.tsx
@@ -1,9 +1,36 @@
+import { getReceiptById } from "@/features/rent-receipt/db";
+import { getTranslations } from "next-intl/server";
+import { RentReceiptPreview } from "@/features/rent-receipt/components/RentReceiptPreview";
+import { Card } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
+import { rentReceiptStatusVariants } from "@/features/rent-receipt/constants";
 
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+export default async function Page({ params }: { params: { id: string } }) {
+    const { id } = params;
+    const t = await getTranslations('rent-receipts');
+    const receipt = await getReceiptById(id);
 
-    const id = (await params).id
-
+    if (!receipt) {
+        throw new Error(t('detail.not-found'));
+    }
+    
     return (
-        <div>Receipt {id}</div>
+        <div className="container p-6 space-y-6">
+            <div className="flex items-center justify-between">
+                <h1 className="text-2xl font-semibold">
+                    {receipt.property.title} - {receipt.tenant.firstName} {receipt.tenant.lastName}
+                </h1>
+                <Badge className={cn(
+                    rentReceiptStatusVariants[receipt.status],
+                    "font-medium shadow-none"
+                )}>
+                    {t('status.' + receipt.status)}
+                </Badge>
+            </div>
+            <Card className="p-6">
+                <RentReceiptPreview receipt={receipt} />
+            </Card>
+        </div>
     )
 }
